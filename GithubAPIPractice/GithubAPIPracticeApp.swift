d1201bb93340698761f317d8f3d51c1cba394c11
Joseph Ollis <35369747+DasTib@users.noreply.github.com>
2023-07-10 00:10:25 -0500

    Initial Commit

---
+++ GithubAPIPractice/GithubAPIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAPIPracticeApp.swift
+//  GithubAPIPractice
+//
+//  Created by Joseph Ollis on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubAPIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
